307795e13f7d797be81adef86bbc30bbf9c062bb
marvelph <marvel@programmershigh.org>
2013-03-03 14:01:20 +0900

    Remove flicker

--- src/Console.java
+++ src/Console.java
@@ -1,8 +1,17 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Console {
 
@@ -14,45 +23,45 @@ public class Console {
     public static final char UP = KeyEvent.VK_UP;
     public static final char DOWN = KeyEvent.VK_DOWN;
 
-    private static volatile ConsoleFrame frame = null;
+    private static volatile Screen screen = null;
 
     public static void locate(int x, int y) {
         init();
 
-        frame.setX(x);
-        frame.setY(y);
+        screen.setX(x);
+        screen.setY(y);
     }
 
     public static void print(char c) {
         init();
 
-        frame.print(c);
+        screen.print(c);
     }
 
     public static void print(String str) {
         init();
 
         for (int index = 0; index < str.length(); index++) {
-            frame.print(str.charAt(index));
+        	screen.print(str.charAt(index));
         }
     }
 
     public static void clear() {
         init();
 
-        frame.clear();
+        screen.clear();
     }
 
     public static char scan(int x, int y) {
         init();
 
-        return frame.scan(x, y);
+        return screen.scan(x, y);
     }
 
     public static boolean pressed(char keyCode) {
         init();
 
-        return frame.pressed(keyCode);
+        return screen.pressed(keyCode);
     }
 
     public static void sleep(int ms) {
@@ -64,11 +73,11 @@ public class Console {
     }
 
     private static void init() {
-        if (frame == null) {
+        if (screen == null) {
             try {
                 SwingUtilities.invokeAndWait(new Runnable() {
                     public void run() {
-                        frame = new ConsoleFrame();
+                    	screen = new Screen();
                     }
                 });
             }
@@ -79,7 +88,7 @@ public class Console {
         }
     }
 
-    private static class ConsoleFrame extends JFrame {
+    private static class Screen {
 
         private static final int FONT_SIZE = 12;
         private static final int KEY_BUFFER_SIZE = 256;
@@ -88,29 +97,66 @@ public class Console {
         private int x;
         private int y;
         private boolean[] keyBuffer;
+        private JFrame frame;
+        private Content content;
 
-        public ConsoleFrame() {
-            super("Screen");
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setVisible(true);
+        public class Content extends JComponent {
 
+        	private Font font;
+        	private int width;
+        	private int height;
+        	private int ascent;
+
+        	public Content() {
+        		super();
+
+        		this.font = new Font(Font.MONOSPACED, Font.PLAIN, FONT_SIZE);
+        		BufferedImage image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+        		Graphics graphics = image.getGraphics();
+        		graphics.setFont(font);
+        		FontMetrics metrics = graphics.getFontMetrics();
+
+                this.width = metrics.charWidth('W');
+                this.height = metrics.getHeight();
+                this.ascent = metrics.getAscent();
+                this.setPreferredSize(new Dimension(Console.WIDTH * this.width, Console.HEIGHT * this.height));
+        	}
+
+        	@Override
+        	  public void paintComponent(Graphics g) {
+                ((Graphics2D)g).setBackground(Color.black);
+                g.clearRect(0, 0, getWidth(), getHeight());
+
+                g.setFont(this.font);
+                g.setColor(Color.white);
+
+                for (int y = 0; y < Console.HEIGHT; y++) {
+                    for (int x = 0; x < Console.WIDTH; x++) {
+                        g.drawChars(Screen.this.screenBuffer[y], x, 1, x * this.width, y * this.height + this.ascent);
+                    }
+                }
+        	}
+
+        }
+
+        public Screen() {
             this.screenBuffer = new char[Console.HEIGHT][Console.WIDTH];
             this.keyBuffer = new boolean[KEY_BUFFER_SIZE];
 
-            Insets insets = getInsets();
-            setFont(new Font(Font.MONOSPACED, Font.PLAIN, FONT_SIZE));
-            FontMetrics metrics = getGraphics().getFontMetrics();
-            int width = insets.left + Console.WIDTH * metrics.charWidth('W') + insets.right;
-            int height = insets.top + Console.HEIGHT * metrics.getHeight() + insets.bottom;
-            setSize(width, height);
+        	this.frame = new JFrame("Screen");
+        	this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        	this.content = new Content();
+        	this.frame.add(this.content);
+        	this.frame.pack();
+        	this.frame.setVisible(true);
 
-            addKeyListener(new KeyAdapter() {
+        	this.frame.addKeyListener(new KeyAdapter() {
 
                 @Override
                 public void keyPressed(KeyEvent e) {
                     int keyCode = e.getKeyCode();
                     if (0 <= keyCode && keyCode < KEY_BUFFER_SIZE) {
-                        ConsoleFrame.this.keyBuffer[keyCode] = true;
+                        Screen.this.keyBuffer[keyCode] = true;
                     }
                 }
 
@@ -118,7 +164,7 @@ public class Console {
                 public void keyReleased(KeyEvent e) {
                     int keyCode = e.getKeyCode();
                     if (0 <= keyCode && keyCode < KEY_BUFFER_SIZE) {
-                        ConsoleFrame.this.keyBuffer[keyCode] = false;
+                    	Screen.this.keyBuffer[keyCode] = false;
                     }
                 }
             });
@@ -150,7 +196,7 @@ public class Console {
                 }
             }
 
-            super.repaint();
+            this.content.repaint();
         }
 
         public void clear() {
@@ -160,7 +206,7 @@ public class Console {
                 }
             }
 
-            super.repaint();
+            this.content.repaint();
         }
 
         public char scan(int x, int y) {
@@ -179,24 +225,6 @@ public class Console {
             }
         }
 
-        @Override
-        public void paint(Graphics g) {
-            ((Graphics2D)g).setBackground(Color.black);
-            g.clearRect(0, 0, getWidth(), getHeight());
-
-            Insets insets = getInsets();
-            setFont(new Font(Font.MONOSPACED, Font.PLAIN, FONT_SIZE));
-            FontMetrics metrics = g.getFontMetrics();
-            int width = metrics.charWidth('W');
-            int height = metrics.getHeight();
-            int ascent = metrics.getAscent();
-            g.setColor(Color.white);
-
-            for (int y = 0; y < Console.HEIGHT; y++) {
-                for (int x = 0; x < Console.WIDTH; x++) {
-                    g.drawChars(this.screenBuffer[y], x, 1, insets.left + x * width, insets.top + y * height + ascent);
-                }
-            }
-        }
     }
+
 }
